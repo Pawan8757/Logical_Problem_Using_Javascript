30a6c1d67f170cb8c8ed580eebf2e067bb4a9559
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-04-08 22:18:14 +0530

    adding Week problem

---
+++ Num_To_Week.js
@@ -0,0 +1,32 @@
+let num = Math.floor(Math.random()*10)%7;
+console.log("here i am going to print week");
+console.log("........................");
+
+if(num==0)
+{
+    console.log("Sunday");
+}
+if(num==1)
+{
+    console.log("Monday");
+}
+if(num==2)
+{
+    console.log("Tuesday");
+}
+if(num==3)
+{
+    console.log("Wednesday");
+}
+if(num==4)
+{
+    console.log("Thursday");
+}
+if(num==5)
+{
+    console.log("Friday");
+}
+if(num==6)
+{
+    console.log("Saturday");
+}
