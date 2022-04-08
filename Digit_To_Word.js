7b6e4b9d893fb14d721e8ce0a3117f3e5be9765e
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-04-08 22:19:12 +0530

    adding digit to word conversion

---
+++ Digit_To_Word.js
@@ -0,0 +1,45 @@
+let num=Math.floor(Math.random()*10)%10;
+
+console.log("here i am converting num to word");
+console.log("...............");
+
+if(num==1)
+{
+    console.log("It's one");
+}
+else if(num==2)
+{
+    console.log("it's two");
+}
+else if(num==3)
+{
+    console.log("it's three");
+}
+else if(num==4)
+{
+    console.log("it's four");
+}
+else if(num==5)
+{
+    console.log("it's five");
+}
+else if(num==6)
+{
+    console.log("it's six");
+}
+else if(num==7)
+{
+    console.log("it's seven");
+}
+else if(num==8)
+{
+    console.log("it's eight");
+}
+else if(num==9)
+{
+    console.log("it's nine");
+}
+else 
+{
+    console.log("ZERO");
+}
