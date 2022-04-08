46b61f1d889cc75433c6f325125934f8af789b56
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-04-08 22:16:57 +0530

    adding max and min problem

---
+++ Max_Min.js
@@ -0,0 +1,36 @@
+let x=124;
+let y=452;
+let z=412;
+let minimum;
+let maximum;
+
+
+console.log("I am doing here max and min program");
+
+if(x>y && x>z)
+{
+    console.log("x is the maximum here");
+}
+else if(y>x && y>z)
+{
+    console.log("y is the maximum here");
+
+}
+else
+{
+    console.log("z is the maximum here");
+
+}
+
+if(y>x && z>x)
+{
+    console.log("x is the minimum");
+}
+else if(z>y && z>y)
+{
+    console.log("y is the minimum ");
+}
+else 
+{
+    console.log("z is the minimum");
+}
