36c5b59ac093ac70b06e8e3fd4b8bafc126f7503
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-04-16 15:21:28 +0530

    adding random digit find out program

---
+++ Random_Digit.js
@@ -0,0 +1,19 @@
+let x=Math.floor(Math.random()*70)+11;
+console.log(x);
+
+let y=Math.floor(Math.random()*68)+11;
+console.log(y);
+
+let z=Math.floor(Math.random()*66)+11;
+console.log(y);
+
+let za=Math.floor(Math.random()*56)+10;
+console.log(y);
+
+let c=Math.floor(Math.random()*55)+33;
+
+let v=x+y+z+za+c;
+console.log(v);
+let l=v/5;
+console.log(l);
+
