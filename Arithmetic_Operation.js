6e6873a78214ac933106f4eddb5e8bebc01ac290
Pawan8757 <pawanyadavrfj2@gmail.com>
2022-04-16 15:17:04 +0530

    adding arithmetic operation program

---
+++ Arithmetic_Operation.js
@@ -0,0 +1,10 @@
+let a=20;
+let b=10;
+let c=5;
+
+let X1=(a+b*c);
+let X2=(c+a/b);
+let X3=(a%b+c);
+let X4=(a*b+c);
+
+console.log(" 1st arithmetic value = " +X1+ "\n 2nd arithmetic value= " +X2+ "\n 3rd arithmetic value = " +X3+ "\n 4th arithmetic value = "+X4);
